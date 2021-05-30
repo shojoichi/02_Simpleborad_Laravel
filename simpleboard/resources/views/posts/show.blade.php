0e1f34189630a106155a69cec85f99b00e52d567
EC2 Default User <ec2-user@ip-172-31-0-86.us-east-2.compute.internal>
2021-05-30 09:38:36 +0000

    using layouts & add header footer

--- simpleboard/resources/views/posts/show.blade.php
+++ simpleboard/resources/views/posts/show.blade.php
@@ -1,8 +1,12 @@
-@if (session('message'))
-    {{ session('message') }}
-@endif
+@extends('layouts.layouts')
 
-{{ $post->title }}
-{{ $post->content }} 
+@section('title', 'Simple Board')
 
-<a href="/posts/{{ $post->id }}/edit">Edit</a>
+@section('content')
+    @if (session('message'))
+        {{ session('message') }}
+    @endif
+
+    {{ $post->title }}
+    {{ $post->content }}
+@endsection 
