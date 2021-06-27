a26077116e7b1aa7b70dd2d17169d424f2c56dbb
EC2 Default User <ec2-user@ip-172-31-0-86.us-east-2.compute.internal>
2021-06-27 07:56:53 +0000

    deploy to heroku

--- simpleboard/resources/views/layouts/layouts.blade.php
+++ simpleboard/resources/views/layouts/layouts.blade.php
@@ -16,6 +16,6 @@
         @component('components.footer')
         @endcomponent
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
